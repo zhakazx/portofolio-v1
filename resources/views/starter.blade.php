504deb40d038e39f0041a8460e2d5c23687b4ab0
Zhaka-Hidayat <zhakazx@gmail.com>
2022-12-10 19:54:31 +0800

    feat: initial commit

---
+++ resources/views/starter.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>ZhakaZx</title>
+
+        @vite('resources/css/app.css')
+    </head>
+    <body class="bg-base-200">
+        
+    </body>
+</html>
